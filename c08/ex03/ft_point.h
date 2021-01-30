b07a3305ba482f20f900f109285c384c8850dc21
tpereira <tpereira@student.42lisboa.com>
2021-01-30 18:59:32 +0000

    ex03

---
+++ c08/ex03/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpereira <tpereira@42lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/30 17:01:28 by tpereira          #+#    #+#             */
+/*   Updated: 2021/01/30 18:53:07 by tpereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef	struct	s_point
+{
+	int x;
+	int y;
+}				t_point;
+
+void			set_point(t_point *point);
+
+#endif
